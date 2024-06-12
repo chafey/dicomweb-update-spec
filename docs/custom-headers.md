9b0974d08ad69b700c5cbf9b59fa1761ded176b7
Chris Hafey <cehafey@amazon.com>
2024-06-12 09:29:21 -0500

    .

---
+++ docs/custom-headers.md
@@ -0,0 +1,9 @@
+Custom Headers
+--------------
+
+The following HTTP Headers may be used to further qualify patients/studies/series/instances according to [RFC 6648](https://datatracker.ietf.org/doc/html/rfc6648)
+
+- DICOMIssuerOfPatientID
+- DICOMPatientName
+- DICOMPatientID
+
--- test/move-instance.test.js
+++ test/move-instance.test.js
@@ -4,15 +4,15 @@ const http = require('./http')
 
 describe('Move Instance', function () {
   describe('Happy Paths', function () {
-    it('should create new patient', async function () { });
-    it('should create new patient and study', async function () { });
-    it('should create new patient, study and series', async function () { });
-    it('should move the instance to an existing patient/study/series', async function () { });
-    it('should move the instance to an existing patient/study/series and change the sop instance uid', async function () { });
+    it('TODO: should create new patient', async function () { });
+    it('TODO: should create new patient and study', async function () { });
+    it('TODO: should create new patient, study and series', async function () { });
+    it('TODO: should move the instance to an existing patient/study/series', async function () { });
+    it('TODO: should move the instance to an existing patient/study/series and change the sop instance uid', async function () { });
   });
   describe('failure scenarios', function () {
-    it('should throw if patient id not provided', async function () { });
-    it('should throw if study uid not provided', async function () { });
-    it('should throw if series uid not provided', async function () { });
+    it('TODO: should throw if patient id not provided', async function () { });
+    it('TODO: should throw if study uid not provided', async function () { });
+    it('TODO: should throw if series uid not provided', async function () { });
   });
 });
